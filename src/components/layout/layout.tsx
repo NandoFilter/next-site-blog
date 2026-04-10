69ca2d09db9b05767c35cd0683304810fe629370
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2026-04-09 22:31:14 -0300

    FEAT: Create Hero Section

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={`${inter.className} relative flex min-h-screen flex-col dark`}>
+    <div className={`${inter.className} relative flex min-h-screen flex-col dark bg-gray-700`}>
       <Header />
 
       <main className="flex-1 flex flex-col">
