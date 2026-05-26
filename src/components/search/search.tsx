56a10f0bdb83a181666cae40d0994dad7bc42ef0
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2026-05-26 20:20:18 -0300

    FEAT: Finish Search

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,11 +1,11 @@
 import { cn } from "@/lib/utils"
-import { SearchIcon } from "lucide-react"
+import { CircleXIcon, SearchIcon } from "lucide-react"
 import { useRouter } from "next/router"
 import { useCallback } from "react"
 
 export const Search = () => {
   const router = useRouter()
-  const query = router.query.q as string
+  const query = (router.query.q as string) ?? ''
 
   const handleSearch = useCallback((event: React.FormEvent) => {
     event.preventDefault()
@@ -24,10 +24,17 @@ export const Search = () => {
     })
   }
 
+  const resetSearch = () => {
+    router.push('/blog', undefined, {
+      shallow: true,
+      scroll: false
+    })
+  }
+
   return (
-    <form onSubmit={handleSearch} className="relative group">
+    <form onSubmit={handleSearch} className="relative group w-full md:w-60">
       <SearchIcon className={cn(
-          'text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300',
+          'absolute left-3 top-1/2 -translate-y-1/2 text-gray-300 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300',
           query ? 'text-blue-300' : ''
         )}
       />
@@ -37,8 +44,15 @@ export const Search = () => {
         placeholder="Buscar"
         value={query}
         onChange={handleQueryChange}
-        className="h-10 w-72 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
+        className="h-10 w-full md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
       />
+
+      {query && (
+        <CircleXIcon
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-300 h-4 w-4 cursor-pointer"
+          onClick={resetSearch}
+        />
+      )}
     </form>
   )
 }
